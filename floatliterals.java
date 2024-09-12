b9f2d6046b5d788e6403a3174087408db057feb9
Sujeetsingh1912 <sujeetsingh59779@gmail.com>
2024-09-12 23:59:01 +0530

    Update floatliterals.java

--- floatliterals.java
+++ floatliterals.java
@@ -1,11 +1,10 @@
-public class floatliterals {
-
-
-   public static void main(String[] args) {
-      
-       float smarksAggregate = 7.8f;
+public class floatliterals  
+{
+ public static void main(String[] args)
+{
+      float smarksAggregate = 7.8f;
        System.out.println("Student Aggreegate Marks   : "+smarksAggregate);
        float temp = 28.5F;
        System.out.println("Temp   : "+temp);
-   }   
+}   
 }
