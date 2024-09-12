69c79626715ac8f35e3f721aaeca7778e0ad1e77
Sujeetsingh1912 <sujeetsingh59779@gmail.com>
2024-09-12 23:58:11 +0530

    Add files via upload

---
+++ floatliterals.java
@@ -0,0 +1,11 @@
+public class floatliterals {
+
+
+   public static void main(String[] args) {
+      
+       float smarksAggregate = 7.8f;
+       System.out.println("Student Aggreegate Marks   : "+smarksAggregate);
+       float temp = 28.5F;
+       System.out.println("Temp   : "+temp);
+   }   
+}
