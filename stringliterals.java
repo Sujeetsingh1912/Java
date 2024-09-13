18eebd925b7a74288795f290f1bb0735f95c2136
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-13 17:19:08 +0530

    Add files via upload

---
+++ stringliterals.java
@@ -0,0 +1,21 @@
+public class stringliterals
+ {
+      public static void main(String[] args) 
+   {
+      
+       String userName = "Durga";
+       String userpassword = "durga123";
+       String userEmail = "durga123@durgasoft.com";
+       String userMobile = "91-9988776655";
+
+
+       System.out.println("User Details");
+       System.out.println("----------------------");
+       System.out.println("User Name         : "+userName);
+       System.out.println("User papssword    : "+userpassword);
+       System.out.println("User Email Id     : "+userEmail);
+       System.out.println("User Mobile No    : "+userMobile);
+
+
+   }   
+}
