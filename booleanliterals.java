59755d0feb6e0a8a4fb677cecd2cfefbaef20328
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-13 17:18:20 +0530

    Add files via upload

---
+++ booleanliterals.java
@@ -0,0 +1,13 @@
+public class booleanliterals
+{
+  public static void main(String[] args)
+   {
+      
+       boolean b1 = true;
+       boolean b2 = false;
+
+
+       System.out.println("Today is SUNDAY : "+b1);
+       System.out.println("This is Holiday : "+b2);
+   }   
+}
