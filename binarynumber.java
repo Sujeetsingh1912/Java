6fd8c2d2b343912c45c3353217790177fe2c712e
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-14 13:01:29 +0530

    Add files via upload

---
+++ binarynumber.java
@@ -0,0 +1,15 @@
+public class binarynumber
+{
+        public static void main(String[] args)
+    {
+        int a = 10;
+        System.out.println(a);
+        int b = 0b10;
+        System.out.println(b);
+        int c = 0b1010;
+        System.out.println(c);
+        int d = 0B1111;
+        System.out.println(d); 
+
+    }
+}
---
+++ octalnumber.java
@@ -0,0 +1,14 @@
+public class Test {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+
+        int b = 010;
+        System.out.println(b);
+
+        int c = 0123;
+        System.out.println(c);
+
+    }
+}
+
