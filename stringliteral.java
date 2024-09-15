2c7b36d2028d80f00996a027cc59255c15e2a847
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-15 20:32:37 +0530

    Add files via upload

---
+++ stringliteral.java
@@ -0,0 +1,25 @@
+public class stringliteral
+ {
+   public static void main(String[] args) 
+   {
+      
+       String str1 = "Durga Software Solutions";
+       System.out.println(str1);
+       String str2 = "Durga 'Software' Solutions";
+       System.out.println(str2);
+       String str3 = "Durga \"Software\" Solutions";
+       System.out.println(str3);
+
+       String str4 = "Durga 'Software' \"Solutions\" ";
+       System.out.println(str4);
+
+        double d = 12_34_56_789.34567;
+        System.out.println(d);        
+
+ 
+
+
+
+
+   }   
+}
