435911a63ae2e0634e24bf082791fb89064b1370
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-15 20:38:30 +0530

    Add files via upload

---
+++ hexadecimalnumber.java
@@ -0,0 +1,15 @@
+public class hexadecimalnumber {
+    public static void main(String[] args) {
+        
+        int a = 10;
+        System.out.println(a);
+
+        int b = 0x10;
+        System.out.println(b);
+
+        int c = 0X123;
+        System.out.println(c);
+
+
+    }
+}
