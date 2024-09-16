c61ca667174e2873bbd9356fe762db258c66b581
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-16 17:56:52 +0530

    Add files via upload

---
+++ Arithmaticoperation.java
@@ -0,0 +1,13 @@
+public class Arithmaticoperation {
+
+
+   public static void main(String[] args) {
+      
+       int a = 0b1010;//a=10
+       int b = 05;// b = 5
+       System.out.println("ADD    : "+(a+b));// ADD : 15
+       System.out.println("SUB    : "+(a-b));// SUB : 5
+       System.out.println("MUL    : "+(a*b));// MUL : 50
+       System.out.println("DIV    : "+(a/b));// DEV : 2
+   }   
+}
