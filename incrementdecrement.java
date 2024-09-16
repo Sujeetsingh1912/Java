86ee47bb350258b20f99c12574c4161ca75e82ef
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-16 18:05:25 +0530

    Add files via upload

---
+++ incrementdecrement.java
@@ -0,0 +1,22 @@
+public class incrementdecrement
+{
+ public static void main(String[] args) 
+   {
+      
+       int a = 5;
+       System.out.println(++a);
+      
+       int b = 5;
+       System.out.println(b++);
+
+
+       int c = 5;
+       System.out.println(--c);
+
+
+       int d = 5;
+       System.out.println(d--);
+
+
+   }   
+}
