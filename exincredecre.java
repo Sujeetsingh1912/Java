86d6bd73f416dea6ec35f4014531be157bcc4822
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-17 22:27:15 +0530

    Add files via upload

---
+++ exincredecre.java
@@ -0,0 +1,30 @@
+public class exinredecre {
+
+
+   public static void main(String[] args) {
+      
+      int a = 7;
+      System.out.println(++a - ++a);
+
+  
+      int b = 8;
+      System.out.println(b++-b++);
+
+      int c = 7;
+      System.out.println(--c+c++-++c);
+
+      int d = 6;
+      System.out.println(--d+d--*++d-++d);
+    
+       
+      int e = 5;
+      System.out.println(--e*--e+e++*--e);
+
+       int f = 6;
+      System.out.println((--f+--f)+(++f-++f)*(++f-f--)-(--f+f++));
+
+
+
+
+   }   
+}
