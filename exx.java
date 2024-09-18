e6624a430ecd52cbba20873df635dd3d5aa730a1
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-18 17:21:13 +0530

    Add files via upload

---
+++ exx.java
@@ -0,0 +1,13 @@
+public class exx {
+
+
+   public static void main(String[] args) {
+       int a = 10;
+       int b = 2;
+       System.out.println(a&b);
+       System.out.println(a|b);
+       System.out.println(a^b);
+       System.out.println(a<<b);
+       System.out.println(a>>b);
+   }
+}
