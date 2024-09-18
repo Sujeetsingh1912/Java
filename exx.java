f16fd2fa94209028a5ba7e1c5ddbaf04060725d5
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-18 17:21:54 +0530

    Update exx.java

--- exx.java
+++ exx.java
@@ -1,7 +1,7 @@
-public class exx {
-
-
-   public static void main(String[] args) {
+public class exx
+{
+   public static void main(String[] args)
+   {
        int a = 10;
        int b = 2;
        System.out.println(a&b);
