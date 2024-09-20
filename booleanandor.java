95abb92c9d987898d19a59d93adcdb0f6af6ceba
Sujeet singh <sujeetsingh59779@gmail.com>
2024-09-20 22:07:22 +0530

    Add files via upload

---
+++ booleanandor.java
@@ -0,0 +1,14 @@
+public class booleanandor
+{
+   public static void main(String[] args) 
+   
+   {
+       int a = 10;
+       int b = 2;
+       System.out.println(a&b);
+       System.out.println(a|b);
+       System.out.println(a^b);
+       System.out.println(a<<b);
+       System.out.println(a>>b);
+   }
+}
