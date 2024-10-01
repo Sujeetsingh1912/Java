7ff9fe1840044d9dfb5976d7f8a35ef83bab04a5
Sujeet singh <sujeetsingh59779@gmail.com>
2024-10-01 06:51:38 +0530

    Update Literals.java

--- Literals.java
+++ Literals.java
@@ -7,7 +7,6 @@ public class Literals
        System.out.println("Durga\nSoftware\nSolutions");
   {  
 
-
        long mobileNo = 9988776655L;
        System.out.println(mobileNo);
        long balance = 12345777899876l;
