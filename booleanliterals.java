92f37d3c3ee2fb83e55205bd5094ec13a244b2bc
Sujeet singh <sujeetsingh59779@gmail.com>
2024-10-17 14:02:07 +0530

    Update booleanliterals.java

--- booleanliterals.java
+++ booleanliterals.java
@@ -1,7 +1,6 @@
 public class booleanliterals {
   public static void main(String[] args)
    {
-
        boolean b1 = true;
        boolean b2 = false;
        System.out.println("Today is SUNDAY : "+b1);
