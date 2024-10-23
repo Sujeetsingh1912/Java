8b958c94106c768a098be2105df89c7093c9baa8
Sujeet singh <sujeetsingh59779@gmail.com>
2024-10-23 20:44:07 +0530

    Update Literals.java

--- Literals.java
+++ Literals.java
@@ -1,7 +1,5 @@
-public class Literals
-   {
+public class Literals {
    public static void main(String[] args) 
-
    {
        System.out.println("DurgaSoftwareSolutions");
        System.out.println("Durga\tSoftware\tSolutions");
