7e2a7029fa5b4c455e68d80b02933e17785fd45f
Sujeet singh <sujeetsingh59779@gmail.com>
2024-12-01 22:23:49 +0530

    Add files via upload

---
+++ Singledimention.java
@@ -0,0 +1,9 @@
+package Arrays;
+public class Singledimention {
+    public  static void main(String[] args) {
+     int[] a =  new int[5];
+     a[0] = 10;
+     a[1] = 20;
+
+}
+}
---
+++ Twodimension.java
@@ -0,0 +1,6 @@
+ class Twodimension {
+    public static void main(String[] args) {
+     int[][] a = {{10,20,20},{40,50},{60}};
+     System.out.print(a);
+}
+}
