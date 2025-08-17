dda5744c43900c2e8dfb3eca79dded6d15588b48
Sujeet singh <sujeetsingh59779@gmail.com>
2025-08-17 09:18:18 +0530

    Update Arithmaticoperation.java

--- Arithmaticoperation.java
+++ Arithmaticoperation.java
@@ -7,9 +7,19 @@ public class Arithmaticoperation {
       
        int a = 0b1010;//a=10
        int b = 05;// b = 5
+
+
+
+
+
+
+
+
+      
        System.out.println("ADD    : "+(a+b));// ADD : 15
        System.out.println("SUB    : "+(a-b));// SUB : 5
        System.out.println("MUL    : "+(a*b));// MUL : 50
        System.out.println("DIV    : "+(a/b));// DEV : 2
    }   
 }
+
