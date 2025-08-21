a759397093db0d032676ae695200db5ae24bf934
Sujeet singh <sujeetsingh59779@gmail.com>
2025-08-21 17:25:49 +0530

    Update Biggestno.java

--- Biggestno.java
+++ Biggestno.java
@@ -4,9 +4,22 @@ public class Biggestno {
         int y = 225;
         int z = 80;
         int big   = x > y?(x > z? x : z): (y > z? y : z);
+
+
+
+
+
+
+
+
+
+
+
+        
         int small = x < y?(x < z? x : z ): (y < z? y : z);
 
         System.out.println("biggest no  :  "+big);
         System.out.println("smallest no  :  "+small);
     }
+
 }
